503603ac38d85462b8ebf7465cd19e25e72c87af
Fabrisio Pereira Bez Fontana Filho <fabrisiopbf@gmail.com>
2021-07-20 17:01:52 -0300

    AttCorrigido

--- AtividadeScanner.java
+++ AtividadeScanner.java
@@ -1,23 +1,23 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class AtividadeScanner {
 
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<String>();
-        Integer i = 0;
-        String items;
-        while(i<5){
-            System.out.println("Adicionar o item " + i + "a lista:");
-            items = ler.nextLine();
-            list.add(items);
-            i++;
-        }
+        try(Scanner ler = new Scanner(System.in);) {
+            List<String> list = new ArrayList<String>();
+            String items;
+                while(list.size()<5){
+                    System.out.println("Adicionar o item " + i + "a lista:");
+                    items = ler.nextLine();
+                    list.add(items);
+                }
         System.out.println(Arrays.toString(list.toArray()));
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
+    
 }
