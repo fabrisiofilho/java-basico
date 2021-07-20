ef847c56eea7449214d28abecf2e4b346381fba4
Fabrisio Pereira Bez Fontana Filho <fabrisiopbf@gmail.com>
2021-07-20 14:08:33 -0300

    Atualizacao20-07

--- Programa.java
+++ Programa.java
@@ -1,34 +1,18 @@
-import java.util.Scanner;
+import java.util.Objects;
 
 public class Programa {
-    public static void main(String[] args) {
-        System.out.println("Seja Bem-Vindo");
-        try (Scanner ler = new Scanner(System.in)) {
-
-            System.out.println("Escreva seu nome:");
-            String numero = ler.next();
-
-            System.out.println("Digite o primeiro numero:");
-            numero = ler.next();
-            verificador(numero);
-            System.out.println("Digite o segundo numero: ");
-            numero = ler.next();
-            verificador(numero);
-            System.out.println("Digite o terceiro numero: ");
-            numero = ler.next();
-            verificador(numero);
-
-        } catch (Exception e) {
-            System.out.println("Deu erro :");
-        }
-    }
-
-    static void verificador(String numero){
-            if ("1".equals(numero)||"2".equals(numero)) {
-                Integer i = Integer.valueOf(numero);
-                System.out.println("Você escreveu o numero: " + i);
-            } else{
-                System.out.println("Você não escreveu um numero:");
+    // [[ytftyfty][tftft][ygygy]]
+    public static void main(String[] args_) {
+        String[] args = { "soma", "1", "8" };
+        System.out.println("Ligou");
+        if (Objects.nonNull(args) && args.length > 0) {
+            System.out.println(args[0]);
+            for (int i = 0; i < args.length; i++) { // i = i + 1 
+                Integer a = Integer.valueOf("1");
+                System.out.println(args[i]);
             }
+        } else {
+            System.out.println("nao veio nada :(");
+        }
     }
 }
