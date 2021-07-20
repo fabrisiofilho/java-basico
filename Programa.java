c85c79c500b8f9dfef02d83f6d219058466f9193
Fabrisio Pereira Bez Fontana Filho <fabrisiopbf@gmail.com>
2021-07-20 14:51:17 -0300

    Atualizacao20-07

--- Programa.java
+++ Programa.java
@@ -3,13 +3,20 @@ import java.util.Objects;
 public class Programa {
     // [[ytftyfty][tftft][ygygy]]
     public static void main(String[] args_) {
-        String[] args = { "soma", "1", "8" };
+        String[] args = {"soma", "1" , "2", "5", "8", "9", "4", "8", "6", "7"};
         System.out.println("Ligou");
         if (Objects.nonNull(args) && args.length > 0) {
             System.out.println(args[0]);
-            for (int i = 0; i < args.length; i++) { // i = i + 1 
-                Integer a = Integer.valueOf("1");
-                System.out.println(args[i]);
+            if ("soma".equals(args[0])) {
+                Integer soma = 0;
+                for (int i = 1; i < args.length; i++) {
+                    soma += Integer.valueOf(args[i]);
+                }
+                System.out.println("A soma dos valores: " + soma.toString());
+            }else{
+                for (String item : args) {
+                    System.out.println(item);
+                }
             }
         } else {
             System.out.println("nao veio nada :(");
